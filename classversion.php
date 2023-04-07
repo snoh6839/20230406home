c4dfe2c14071f49ab09dd491fdfeed99371fa80f
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-04-07 14:40:25 +0900

    Update classversion.php

--- classversion.php
+++ classversion.php
@@ -173,6 +173,71 @@ class Blackjack {
         }
     }
 }
+$blackjack = new Blackjack();
+
+// 플레이어 카드 두 장 받기
+$blackjack->player->addCardToHand($blackjack->deck->drawCard());
+$blackjack->player->addCardToHand($blackjack->deck->drawCard());
+
+// 딜러 카드 한 장 받기
+$blackjack->dealer->addCardToHand($blackjack->deck->drawCard());
+
+// 플레이어 카드 보여주기
+echo "플레이어의 카드: ";
+foreach ($blackjack->player->getHand() as $card) {
+    echo $card->getFace() . $card->getSuit() . " ";
+}
+echo "\n";
+
+// 딜러 카드 보여주기 (첫 번째 카드는 가려둠)
+echo "딜러의 카드: ";
+$blackjack->dealer->showFirstCard();
+echo " ***\n";
+
+// 플레이어가 히트 또는 스탠드 선택하기
+while ($blackjack->player->getHandValue() < 21) {
+    $input = readline("Hit or Stand? (h/s) ");
+    if ($input == 'h') {
+        $blackjack->player->addCardToHand($blackjack->deck->drawCard());
+        echo "플레이어의 카드: ";
+        foreach ($blackjack->player->getHand() as $card) {
+            echo $card->getFace() . $card->getSuit() . " ";
+        }
+        echo "\n";
+    } else {
+        break;
+    }
+}
+
+// 플레이어가 21을 초과했으면 게임 종료
+if ($blackjack->player->getHandValue() > 21) {
+    echo "플레이어 패!\n";
+} else {
+    // 딜러가 17 이상이 될 때까지 카드 받기
+    $blackjack->dealer->play($blackjack->deck);
+
+    // 딜러 카드 보여주기
+    echo "딜러의 카드: ";
+    foreach ($blackjack->dealer->getHand() as $card) {
+        echo $card->getFace() . $card->getSuit() . " ";
+    }
+    echo "\n";
+
+    // 게임 승패 결정
+    $playerValue = $blackjack->player->getHandValue();
+    $dealerValue = $blackjack->dealer->getHandValue();
+    if ($dealerValue > 21 || $playerValue > $dealerValue) {
+        echo "플레이어 승!\n";
+    } elseif ($playerValue < $dealerValue) {
+        echo "딜러 승!\n";
+    } else {
+        echo "무승부!\n";
+    }
+}
+
+// 게임 종료 후 카드 초기화
+$blackjack->player->clearHand();
+$blackjack->dealer->clearHand();
 
 
     ?>
